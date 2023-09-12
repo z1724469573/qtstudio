b311f32cd15545befcd2b0005a1caf58bee020ac
筠dayday <1724469573@qq.com>
2023-09-12 16:33:20 +0800

    feat:初始化招新项目

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	// 一行最多多少个字符
+	printWidth: 150,
+	// 指定每个缩进级别的空格数
+	tabWidth: 2,
+	// 使用制表符而不是空格缩进行
+	useTabs: true,
+	// 在语句末尾是否需要分号
+	semi: false,
+	// 是否使用单引号
+	singleQuote: false,
+	// 更改引用对象属性的时间 可选值"<as-needed|consistent|preserve>"
+	quoteProps: "as-needed",
+	// 在JSX中使用单引号而不是双引号
+	jsxSingleQuote: false,
+	// 多行时尽可能打印尾随逗号， 可选值"<none|es5|all>"，默认none
+	trailingComma: "es5",
+	// 在对象文字中的括号之间打印空格
+	bracketSpacing: true,
+	// jsx 标签的反尖括号需要换行
+	jsxBracketSameLine: false,
+	// 在单独的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x
+	arrowParens: "always",
+	// 这两个选项可用于格式化以给定字符偏移量（分别包括和不包括）开始和结束的代码
+	rangeStart: 0,
+}
---
+++ src/store/user/index.ts
@@ -0,0 +1,17 @@
+// import { ref } from "vue"
+// import { defineStore } from "pinia"
+// // 用户user模块
+// export const useUserStore = defineStore("user", () => {
+//     // token信息
+//     const token = ref("")
+//     // 用户信息
+//     const userInfo = ref({
+//         id: "",
+//         name: "测试员",
+//         phone: "",
+//     })
+//     return {
+//         token,
+//         userInfo
+//     }
+// })
